fc60a845b367f66cee5588a4bf54affcd45d14b2
Woofy <woofy-the-electrocat@outlook.com>
2018-06-12 12:58:45 +0100

    change in /src (source code update)

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -3,26 +3,39 @@ import { render } from "react-dom";
 import MyComponent from "../../lib";
 import "./styles.css";
 
-function Demo() {
-  return (
-    <div>
-      <h1>Demo with examples of the component</h1>
 
-	    <MyComponent
-	       className={"formulaX errorX"}
-	       value={30}
-	       onChange={ ()=>{} /*value => {
-		   props.modifyElem(PROP.key, value);
-	      }*/}
-	      width={136}
-	      sliderSize={15}
-	      sliderColour={"rgba(16, 102, 239, 0.52)"}
-	      slideMin={-200}
-	      slideMax={200}
-	      />
+class Demo extends React.Component {
 
-    </div>
-  );
+    constructor() {
+	super();
+
+	this.state = {
+	    myValue: 30
+	};
+    }
+    
+    render() {
+	return (
+	    <div>
+	      <h1>Demo with examples of the component</h1>
+
+	      <MyComponent
+		 className={"formulaX errorX"}
+		 value={this.state.myValue}
+		 onChange={ value => {
+		     this.setState({myValue: value});
+		}}
+		width={136}
+		sliderSize={15}
+		sliderColour={"rgba(16, 102, 239, 0.52)"}
+		slideMin={-200}
+		slideMax={200}
+		/>
+
+	    </div>
+	);
+    }
 }
+
 
 render(<Demo />, document.getElementById("app"));
